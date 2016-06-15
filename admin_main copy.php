24cac5e56ed2042c49399707761d62429450c47a
thomas skerten <845862661@cit3-502-15m-3.auckland.natcoll.ac.nz>
2016-06-15 15:39:24 +1200

    last commit

---
+++ admin_main copy.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>15ADWE07 Web Journey</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="assets/stylesheets/base.css">
+	<link rel="stylesheet" href="assets/stylesheets/skeleton.css">
+	<link rel="stylesheet" href="assets/stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
+	<link rel="apple-touch-icon" href="assets/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+
+</head>
+<body class="admin">
+
+
+
+	<!-- Primary Page Layout
+	================================================== -->
+
+	<!-- Delete everything in this .container and get started on your own site! -->
+
+	<div class="container">
+		<div class="sixteen columns">
+			<h1 class="remove-bottom">Our Web Journeys - Admin</h1>
+			<h5>The web adventures of 15ADW07</h5>
+			<hr />
+		</div>
+		<div class="one-third column">
+			<ul>
+				<li><h3>About us</h3>
+					<ul>
+						<li><a href="admin_page.html">Our goals</a></li>
+						<li><a href="admin_page.html">What we are like</a></li>
+					</ul>
+				</li>
+				<li><h3>Travellers</h3>
+					<ul>
+						<li><a href="admin_page.html">Charmaine</a></li>
+						<li><a href="admin_page.html">Avee</a></li>
+						<li><a href="admin_page.html">Logan</a></li>
+					</ul>
+				</li>
+				<li><h3>Events</h3>
+					<ul>
+						<li><a href="admin_page.html">Lorem.</a></li>
+						<li><a href="admin_page.html">Dolorem?</a></li>
+						<li><a href="admin_page.html">Enim.</a></li>
+					</ul>
+				</li>
+			</ul>
+			<a href="admin_new_page.html">+ New page</a>
+		</div> 
+		<!-- end of admin header -->
+		
+		<div class="two-thirds column">
+			<h3>Our goal</h3>
+			<p>Although coming from different backgrounds, we are united to become web experts. Here are our stories ...</p>
+			<a href="admin_edit_page.html">+ Edit this page</a>
+		</div>
+
+	</div><!-- container -->
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
